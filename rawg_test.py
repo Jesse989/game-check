fc303b1405dc13290c0791f8283f376f734b9d1d
Jesse Neumann <jesseneumann@gmail.com>
2020-07-12 13:03:03 -0600

    setup unittest

---
+++ rawg_test.py
@@ -0,0 +1,24 @@
+from rawg import RAWG 
+import unittest   
+
+
+class Test_RAWG(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+    def test_something(self):
+        pass
+
+   
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
+# calls api and returns game info
+# rawg = RAWG()
+
+# rawg.get_game('bioshock')
